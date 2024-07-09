be1fe8c2f07aa64fe77879e68b5da209e2d5bd01
Bryan Lopez Rosales <d00442163@CMP-SCC112-1113.local>
2024-07-09 12:19:18 -0600

    whatever

--- model.js
+++ model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const { Schema } = mongoose;
 
-mongoose.connect(process.env.DBPASSWORD);
+mongoose.connect(process.env.DATABASE);
 
 const UserSchema = Schema({
   email: {
